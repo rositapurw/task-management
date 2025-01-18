a99646e68eb95e4e3f6f46d04e18ebf5f8a19665
Rosita Purwaningsih <rositaprwnngsh@gmail.com>
2025-01-18 14:21:09 +0800

    install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { TaskList } from "./components/task-list";
 
 const taskItems = [
-  { id: 1, text: "Mandi", isCompleted: true },
-  { id: 2, text: "Sarapan", isCompleted: true },
-  { id: 3, text: "Bekerja", isCompleted: false },
+  { id: 1, text: "Baca Dokumentasi React", isCompleted: true },
+  { id: 2, text: "Mengerjakan Tugas", isCompleted: true },
+  { id: 3, text: "Belajar Typescript", isCompleted: false },
 ];
 
 export function App() {
   return (
     <div>
       <header>
-        <h1>Task Management</h1>
+        <h1 className="text-3xl font-bold underline">Task Management</h1>
       </header>
 
       <p className="read-the-docs">Application to manage tasks.</p>
